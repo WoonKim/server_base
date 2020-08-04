17be762b88e3c9f4313c42152b2b707bca4bf40c
woonkim <woonkim@live.com.my>
2020-08-04 18:21:38 +0800

    Error handling

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(function (req, res, next) {
 
 app.use(function (err, req, res, next) {
   res.status(err.status || 500);
-  res.json(err.message);
+  res.json(err);
 });
 
 module.exports = app;
--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -1,16 +1,23 @@
 const Joi = require('@hapi/joi');
 const pick = require('../utils/pick');
 const _ = require('lodash');
+const createError = require('http-errors');
 
 const validate = schema => (req, res, next) => {
   const validSchema = pick(schema, ['params', 'query', 'body']);
-  console.log(validSchema);
   const object = pick(req, _.keys(validSchema));
   const { value, error } = Joi.compile(validSchema)
     .prefs({ errors: { label: 'key' } })
     .validate(object);
-  console.log(value);
-  console.log(error);
+
+  req = _.assign(value);
+  if (error) {
+    const message = _.chain(error.details)
+      .map(o => o.message)
+      .join(', ')
+      .value();
+    return next(createError(400, message));
+  }
   next();
 };
 
